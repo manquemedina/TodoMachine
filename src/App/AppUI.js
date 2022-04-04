b48e33c97f8d53ff00cc93bc51be5ccb970a382e
manquemedina <manquemedina1@gmail.com>
2022-04-04 11:16:19 -0300

    implementada la primer mitad del Contexto, repasar antes de seguir porque está verde

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,51 +1,46 @@
 import React from "react";
+import { TodoContext } from "../TodoContext";
 import { TodoCounter } from "../TodoCounter";
 import { TodoItem } from "../TodoItem";
 import { TodoList } from "../TodoList";
 import { TodoSearch } from "../TodoSearch";
 import { CreateTodoButton } from "../CreateTodoButton";
-
-function AppUI({
-  loading,
-  error,
-  totalTodos,
-  completedTodos,
-  searchValue,
-  setSearchValue,
-  searchedTodos,
-  completeTodo,
-  deleteTodo,
-}) {
+//una vez creado nuestro contexto podemos eliminar las props de los componentes Counter y Search, ya que ahora las van a consumir de nuestro TodoContext.Provider
+function AppUI() {
   return (
     <>
-      <TodoCounter total={totalTodos} completed={completedTodos} />
-      {/* pasando estado de papá a hijo */}
+      <TodoCounter />
+      <TodoSearch />
+      {/* El componente TodoList va a consumir nuestros estados loading y error, implementados dentro del custom Hook, y el resto de props (ahora en el index.js de la carpeta TodoContext), entonces lo envolvemos en la etiqueta de .Consumer */}
+      <TodoContext.Consumer>
+        {/* Dentro de la etiqueta consumer en vez de pasar directamente las props lo que hacemos es crear una función que reciba nuestro objeto "value" asignado al .Provider */}
 
-      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
-      {/* componente parent App pasa el estado como props al children TodoSearch */}
-      {/* Acá tenemos que cablear la función completeTodos */}
-      <TodoList>
-        {error && (
-          <p>
-            <strong>🌋ERROR: </strong>Caos Muerte y Destrucción!
-          </p>
+        {({ error, loading, searchedTodos, completeTodo, deleteTodo }) => (
+          <TodoList>
+            {error && (
+              <p>
+                <strong>🌋ERROR: </strong>Caos Muerte y Destrucción!
+              </p>
+            )}
+            {loading && <p>Calmachicha tamo en eso ⏰</p>}
+            {!loading && !searchedTodos.length && (
+              <h5>Crea tu primera tarea!🤓</h5>
+            )}
+            ;
+            {searchedTodos.map((todo) => (
+              <TodoItem
+                key={
+                  todo.text
+                } /* //el atributo "key" debe ser un identificador único para c/item, en éste caso el texto del todo */
+                text={todo.text}
+                completed={todo.completed}
+                onComplete={() => completeTodo(todo.text)}
+                onDelete={() => deleteTodo(todo.text)} //creamos atributo onComplete que va a llamar a la función pasandole su identificador único, todo.text. Este atributo lo pasamos al método onclick de nuestro span✔️ en todoItem
+              />
+            ))}
+          </TodoList>
         )}
-        {loading && <p>Calmachicha tamo en eso ⏰</p>}
-        {!loading && !searchedTodos.length && <h5>Crea tu primera tarea!🤓</h5>}
-
-        {searchedTodos.map((todo) => (
-          <TodoItem
-            key={
-              todo.text
-            } /* //el atributo "key" debe ser un identificador único para c/item, en éste caso el texto del todo */
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)} //creamos atributo onComplete que va a llamar a la función pasandole su identificador único, todo.text. Este atributo lo pasamos al método onclick de nuestro span✔️ en todoItem
-          />
-        ))}
-      </TodoList>
-
+      </TodoContext.Consumer>
       <CreateTodoButton />
     </>
   );
--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import { AppUI } from "./AppUI";
+import { TodoProvider } from "../TodoContext";
 
 /* const todosHardcodea2 = [
   { text: "shambular primero", completed: false },
@@ -9,125 +10,13 @@ import { AppUI } from "./AppUI";
   { text: "garrarlapala", completed: true },
 ]; */
 
-//CUSTOM HOOK p localStorage
-function useLocalStorage(itemName, initialValue) {
-  //Para simular la asincronicidad de una API en el back que tendríamos que esperar que devuelva nuestra data ponemos es parte de la lógica en un setTimeOut
-  //primero traemos nuestro estado arriba y a useState le pasamos initialValue, ya que parsedItem (quepasábamos antes) quedó dentro del timeOut y fuera del scope de nuestro Hook
-  //luego generamos los dos estados nuevos que necesitamos, loading y error
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const [item, setItem] = useState(initialValue);
-  useEffect(() => {
-    setTimeout(() => {
-      //Todo dentro de un bloque try-catch, para que actualice nuestro estado de loading (en el try), y si ocurre algo corre el catch(error)
-      try {
-        //chekea a ver si existe el item
-        const localStorageItem = localStorage.getItem(itemName);
-        let parsedItem;
-        //condicional p que si existe lo parsee y devuelva, y si no lo declare y asigne initialValue
-        if (!localStorageItem) {
-          localStorage.setItem(itemName, JSON.stringify(initialValue));
-          parsedItem = initialValue;
-        } else {
-          parsedItem = JSON.parse(localStorageItem);
-          //Desde dentro del timeOut (fake API) actualizamos el estado con el valor de parsedItem
-          setItem(parsedItem);
-          //actualizamos nuestro estado loading, que inicialmente va a ser true, a false, xq ya cargó wey
-          setLoading(false);
-        }
-      } catch (error) {
-        setError(error);
-      }
-    }, 1000);
-  },[]);
-
-  //función para persistir datos, que vamos a llamar en los metodos complete/delete Todo
-  const saveItem = (newItem) => {
-    try {
-      const stringifyedItem = JSON.stringify(newItem);
-      localStorage.setItem(itemName, stringifyedItem);
-      //nuestro setter del estado, que antes teníamos en <App> y trajimos al custom hook
-      setItem(newItem);
-    } catch (error) {
-      setError(error);
-    }
-  };
-  //retorno de nuestro estado y su func actualizadora
-  return { item, saveItem, loading, error };
-}
-
 function App() {
-  //Lógica para persistencia de datos en localStorage
-
-  //estados//
-  //cuando traemos más de un estado desde un componente ya no comviene hacerlo en un array, sino en un objeto. En éste caso, que al estado de todos/saveTodos que traíamos de nuestro hook, le sumamos el estado de loading y error hacemos esto de traerlo en formato objeto y en ahí mismo renombramos, ya que lo que acá habiamos llamado "todos" nuestro hook nos lo pasa abstraído como "item"
-  const {
-    item: todos,
-    saveItem: saveTodos,
-    loading,
-    error,
-  } = useLocalStorage("TODOS_V1", []);
-  const [searchValue, setSearchValue] = useState(""); //estado levantado desde TodoSearch para que sea accesible por los demas componentes
-
-  //Props para el TodoCounter//
-
-  const totalTodos = todos.length; //cantidad de tareas en nuestro estado
-  const completedTodos = todos.filter((todo) => !!todo.completed).length; //.filter() itera por cada item de nuestro array "todos" (nuestro estado) y la arrow func pasada al metodo filter nos devuelve los items que tengan el atributo completed como "true". El .length del final nos devuelve el largo del array retornado por e .filter, dándonos la cantidad de tareas resueltas.
-  //ahora para implementar filtro de búsqueda:
-
-  let searchedTodos = []; // ahora vamos a usar éste array para mapear los <TodoItem> en nuestro componente <TodoList>
-
-  //lógica para TodoSearch//
-
-  if (searchValue.length <= 0) {
-    searchedTodos = todos; //porque si no buscamos nada nos devuelve la totalidad de tareas
-  } /* else {
-    searchedTodos = todos.filter((todo) => {
-      //con ésta función convertimos cada todo de nuestro searchedTodos[] a minúscula para comparar con el searchValue (esto es para evitar errores)
-      const todoText = todo.text.toLowerCase();
-      //same para el searchValue, notar que no va el ".text" ya que al ser in input text solo puede tener texto
-      const searchText = searchValue.toLowerCase();
-      //la VERDADERA función filtradora con includes
-      return todoText.includes(searchText);
-    }); */
-
-  //este else es same que el de arriba pero + corto y l-gante, en vez de declarar variables para pasar todo a minúscula lo hace directamente con el .includes para flitrar
-  else {
-    searchedTodos = todos.filter((todo) => {
-      return todo.text.toLowerCase().includes(searchValue.toLowerCase());
-    });
-  }
-
-  //Lógica para completar y eliminar Todos//
-
-  //ésta función recibe el texto, que es el identificador único de nuestros todos y genera un nuevo array con TODAS nuestras tareas para reemplazar nuestro estado anterior "todos", pero marcando la propiedad "completed" como true del index que le pasamos a la funcion:
-
-  const completeTodo = (text) => {
-    const todoIndex = todos.findIndex((todo) => todo.text === text);
-    const newTodos = [...todos]; //clonamos array (importante el destructuring)
-    newTodos[todoIndex].completed = true; //seteamos completed de nuestro índice
-    saveTodos(newTodos); //llamamos actualizador de nuestro estado (ahora manejado por el customHook) pasandole nuestro array con el todo actualizado
-  };
-  const deleteTodo = (text) => {
-    const todoIndex = todos.findIndex((todo) => todo.text === text);
-    const newTodos = [...todos];
-    newTodos.splice(todoIndex, 1);
-    saveTodos(newTodos); //llamamos método actualizador de nuestro estado pasandole nuestro array con el todo completado
-  };
-
   // Nuestra UI
+  //al envolver nuestro componente AppUi en el TodoProvider hacemos que cualquier componete dentro de AppUi pueda acceder al estado y props declaradas en nuestro context provider 
   return (
-    <AppUI
-      loading={loading}
-      error={error}
-      totalTodos={totalTodos}
-      completedTodos={completedTodos}
-      searchValue={searchValue}
-      setSearchValue={setSearchValue}
-      searchedTodos={searchedTodos}
-      completeTodo={completeTodo}
-      deleteTodo={deleteTodo}
-    />
+    <TodoProvider>
+      <AppUI />
+    </TodoProvider>
   );
 }
 
