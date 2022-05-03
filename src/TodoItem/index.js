591a417d96eb5f666e6adec493dcf2a060ec553f
unknown <manquemedina1@gmail.com>
2022-05-03 14:11:17 -0300

    hey al final quedó y no lompi nada

--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -1,87 +1,27 @@
 import styled from "styled-components";
 import React from "react";
+import "./TodoItemCSS.css";
 
-const StyledTodoItem = styled.li`
-  display: flex;
-  flex-direction: column;
-  text-align: left;
-  align-items: center;
-  justify-content: stretch;
-  background-color: #666;
-  font-family: "Quicksand", sans-serif;
-  font-weight: 800;
-  border: solid 2px white;
-  border-radius: 10px;
-  color: #fff;
-  opacity: 0.7;
-  margin: 0.5rem 1rem;
-  padding: 1rem;
-  /* height: fit-content; */
-  /* max-width: fit-content; */
-  box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
-  transition: all 0.25s;
-  :hover {
-    background-color: #777;
-    opacity: 1;
-    transform: scale(1.02);
-  }
-  @media (max-width: 592px) {
-    margin: 1rem auto;
-  }
-`;
-const Icondiv = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: row;
-  padding: 1.5rem 1rem 0;
-  align-items: center;
-  justify-content: right;
-  text-align: center;
-  opacity: 0;
-  transition: opacity 0.25s;
-  :hover {
-    opacity: 1;
-  }
-`;
-const Span1 = styled.span`
-  margin: 0 0.2rem;
-  cursor: pointer;
-  width: 2.5rem;
-  padding: 0.25rem;
-  border-radius: 50px;
-  background-color: #999;
-  opacity: 0.7;
-  :hover {
-    opacity: 1;
-    background-color: #1af539;
-  }
-`;
-const P = styled.p`
-  padding: 1rem;
-  margin: 0.25rem;
-`;
-const Span2 = styled.span`
-  margin: 0 0.2rem;
-  cursor: pointer;
-  width: 2.5rem;
-  padding: 0.25rem;
-  border-radius: 50px;
-  background-color: #555;
-  opacity: 0.7;
-  :hover {
-    opacity: 1;
-    background-color: #d62323;
-  }
-`;
+const StyledTodoItem = styled.li``;
+const Icondiv = styled.div``;
+const Span1 = styled.span``;
+const P = styled.p``;
+const Span2 = styled.span``;
 
 export function TodoItem(props) {
   return (
     /* acá va a haber que meter CSS sobre el styled components lo siento BB */
-    <StyledTodoItem className={`${props.done && "TodoItemDone"}`}>
-      <P children={props.text} />
-      <Icondiv>
-        <Span1 onClick={props.onComplete}>✔︎</Span1>
-        <Span2 onClick={props.onDelete}>✘</Span2>
+    <StyledTodoItem
+      className={`StyledTodoItem ${props.done && "StyledTodoItemDone"}`}
+    >
+      <P children={props.text} className={"P"} />
+      <Icondiv className={"IconDiv"}>
+        <Span1 className={"Span1"} onClick={props.onComplete}>
+          ✔︎
+        </Span1>
+        <Span2 className={"Span2"} onClick={props.onDelete}>
+          ✘
+        </Span2>
       </Icondiv>
     </StyledTodoItem>
   );
