0c87fbae65e25cef0ce238177d2b32a388a152b2
unknown <manquemedina1@gmail.com>
2022-05-12 10:44:36 -0300

    segundo deploy

---
+++ src/App/useTodos.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { useLocalStorage } from "./useLocalStorage";
+
+function useTodos() {
+  const {
+    item: todos,
+    saveItem: saveTodos,
+    loading,
+    error,
+  } = useLocalStorage("TODOS_V1", []);
+  const [searchValue, setSearchValue] = React.useState("");
+  const [openModal, setOpenModal] = React.useState(false);
+  const [done, setDone] = React.useState(false);
+
+  const completedTodos = todos.filter((todo) => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if (!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter((todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
+  }
+
+  const addTodo = (text) => {
+    const newTodos = [...todos];
+    newTodos.push({
+      completed: false,
+      text,
+      done: false,
+    });
+    saveTodos(newTodos);
+  };
+
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex((todo) => todo.text === text);
+    const newTodos = [...todos];
+    /* metemos alto toggle acá abajo con el !  */
+    newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
+    newTodos[todoIndex].done = !newTodos[todoIndex].done;
+    saveTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex((todo) => todo.text === text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1);
+    saveTodos(newTodos);
+  };
+  const onSearchValueChange = (event) => {
+    //función handler que escucha valores del input
+    console.log(event.target.value);
+    setSearchValue(event.target.value); //método actualizador de estado
+  };
+  return {
+    loading,
+    error,
+    totalTodos,
+    completedTodos,
+    searchValue,
+    setSearchValue,
+    onSearchValueChange,
+    searchedTodos,
+    addTodo,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal,
+    done,
+    setDone,
+  };
+}
+
+export { useTodos };
